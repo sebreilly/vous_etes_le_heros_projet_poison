06eb173300174d9fa213c6d65b30d3063ea5fdc1
sebreilly <90867276+sebreilly@users.noreply.github.com>
2021-09-24 16:01:51 -0400

    changer le js, pas fini

--- page_web_projet_vous_etes_le_hero/script.js
+++ page_web_projet_vous_etes_le_hero/script.js
@@ -1,9 +1,15 @@
 let chapitre1 = {
 subtitle: "L'éveil",
 text: "Vous vous réveillez dans une cellule de prison. Il n'y a rien autour de vous sauf les barreaux de votre cellule et une fenêtre baricadée. La porte de de votre cellule est fermée.",
-img: "barreaux.jpg"
+img: "barreaux.jpg",
+option: []
 };
 
 let chapterObj = {
-
+slug: ''
 };
+
+function goToChapter(chapterName) {
+let chapitre = chapterObj.slug;
+console.log(chapitre);
+}
