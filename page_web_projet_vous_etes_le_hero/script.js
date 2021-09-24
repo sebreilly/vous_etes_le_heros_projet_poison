a640836f33cefdfb8a0ad0cf1dbba5e49686b191
sebreilly <90867276+sebreilly@users.noreply.github.com>
2021-09-24 12:25:07 -0400

    début js

---
+++ page_web_projet_vous_etes_le_hero/script.js
@@ -0,0 +1,9 @@
+let chapitre1 = {
+subtitle: "L'éveil",
+text: "Vous vous réveillez dans une cellule de prison. Il n'y a rien autour de vous sauf les barreaux de votre cellule et une fenêtre baricadée. La porte de de votre cellule est fermée.",
+img: "barreaux.jpg"
+};
+
+let chapterObj = {
+slug: "nom",
+};
