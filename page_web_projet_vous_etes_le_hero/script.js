d792229409fc2b91fffdb01dac28bab6fa901919
sebreilly <90867276+sebreilly@users.noreply.github.com>
2021-09-29 14:54:13 -0400

    chapitre1-8

--- page_web_projet_vous_etes_le_hero/script.js
+++ page_web_projet_vous_etes_le_hero/script.js
@@ -1,12 +1,97 @@
 let chapitre1 = {
-subtitle: "L'éveil",
-text: "Vous vous réveillez dans une cellule de prison. Il n'y a rien autour de vous sauf les barreaux de votre cellule et une fenêtre baricadée. La porte de de votre cellule est fermée.",
+subtitle: "La porte",
+text: "Vous vous rendez compte que la porte de de votre cellule est vérouiller. Que faîtes-vous?",
 img: "barreaux.jpg",
 option: []
 };
 
+let chapitre2 = {
+subtitle: "Le corridor",
+text: "Vous vous retrouver au millieu d'un corridor, vous ne savez pas quelle direction prendre. Que se passe t'il?",
+img: "corridor.jpg",
+option: [] 
+}
+
+let chapitre3 = {
+subtitle: "L'eau",
+text: "Vous vous rendez compte que le corridor se remplit d'eau. Vous devez agir rapidement. Que faîtes-vous?",
+img: "eau.jpg",
+option: []
+}
+
+let chapitre4 ={
+subtitle: "Le corridor 2.0",
+text: "Vous devez trouver une sortie. Il est temps de prendre une décition. Quel direction prenez-vous?",
+img: "corridor.jpg",
+option: []
+}
+
+let chapitre5 = {
+subtitle: "La figure",
+text: "Un téléphone se met à sonner. Vous décrocher l'apreile et prenez la ligne. Au bout du fil quelqu'un vous parle: « Vous êtes la cible d'une expérience humaine. Si vous voulez survivre, je vous conseil vivement de vous trouver une arme. Nous nous reverons, à vous de décider dans quelle circonstance. Bonne chance.» Vous trouvez ça étrange. Que faîtes-vous?",
+img: "telephone.jpg",
+option: []
+}
+
+let chapitre6 = {
+subtitle: "L'éthique",
+text: "Après avoir assomer l'homme qui a éssayer de vous tuer, vous lui prenez son arme. Vous devez prendre une décition crucial. Que faîtes-vous?",
+img: "couteau.jpg",
+option: []
+}
+
+let chapitre7 = {
+subtitle: "cache-cache",
+text: "Vous devez trouver un endroit pour vous cacher et vite. Où vous cachez-vous",
+img: "cachette.jpg",
+option: []
+}
+
+let chapitre8 = {
+subtitle: "Le retour",
+text: "Vous devez devez trouver un moyen de vous échaper, mais comment la pièce n'a pas d'issu. Que faîtes-vous?",
+img: "mur.jpg",
+option: []
+}
+
+let chapitre9 = {
+subtitle: "La porte",
+text: "Vous vous rendez compte que la porte de de votre cellule est vérouiller. Que faîtes-vous?",
+img: "barreaux.jpg",
+option: []
+}
+
+let chapite10 = {
+subtitle: "La porte",
+text: "Vous vous rendez compte que la porte de de votre cellule est vérouiller. Que faîtes-vous?",
+img: "barreaux.jpg",
+option: []
+}
+
+let chapitre11 = {
+subtitle: "La porte",
+text: "Vous vous rendez compte que la porte de de votre cellule est vérouiller. Que faîtes-vous?",
+img: "barreaux.jpg",
+option: []
+}
+
+let chapitre12 = {
+subtitle: "La porte",
+text: "Vous vous rendez compte que la porte de de votre cellule est vérouiller. Que faîtes-vous?",
+img: "barreaux.jpg",
+option: []
+}
+
+let chapitre13 = {
+subtitle: "La porte",
+text: "Vous vous rendez compte que la porte de de votre cellule est vérouiller. Que faîtes-vous?",
+img: "barreaux.jpg",
+option: []
+}
+
+
 let chapterObj = {
-slug: ''
+porte: chapitre1,
 };
 
 function goToChapter(chapterName) {
